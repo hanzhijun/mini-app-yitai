0fd4a5737f14d507dfbfefafb33309acf2a9f22f
Administrator <512804627@qq.com>
2019-04-28 17:06:27 +0800

    益泰智慧医疗

--- app.js
+++ app.js
@@ -1,83 +1,103 @@
 //app.js
 let Util = require('/utils/storage.js');
+let socketMsgQueue = [];
+let isLoading = false;
 
 App({
-    onLaunch: function () {
-    },
-    /**
-     * 基础数据
-     */
-    globalData: {
-        vision: '1.0.0',
-        host: 'https://api.darma.cn/mattress',
-        userInfo: null, // {"accessToken":"71bc9ce1c95547459da2f31801f97a08","tokenType":"default","username":"ether"}
-        isLogin: null,
-        deviceNos: '641743000773' // 设备号 641743000773
-    },
-    /**
-     * 公共异步 一级接口 (不需登录)
-     * @param type 类型： post,get,put,delete
-     * @param url 接口地址
-     * @param data 参数
-     * @param res 成功
-     * @param reg 失败
-     */
-    myAjax(type, url, data, res, reg) {
-        wx.request({
-            url: this.globalData.host + url,
-            data: data,
-            header: {
-                'content-type': 'application/json'
-            },
-            method: type,
-            success(data) {
-                res && res(data.data)
-            },
-            fail(data) {
-                reg && reg(data.data)
-            }
-        })
-    },
-    /**
-     * 公共异步 二级接口 (需登录)
-     * @param type 类型： post,get,put,delete
-     * @param url 接口地址
-     * @param data 参数
-     * @param res 成功
-     * @param reg 失败
-     */
-    myAjax2(type, url, data, res, reg) {
-        let accessToken = Util.getCookie('accessToken');
-        if (!accessToken) {
-            wx.redirectTo({
-                url: '../login/login'
-            })
-        }
-        wx.request({
-            url: this.globalData.host + url,
-            data: data,
-            header: {
-                'content-type': 'application/json',
-                'accessToken': accessToken
-            },
-            method: type,
-            success(data) {
-                res && res(data.data)
-            },
-            fail(data) {
-                reg && reg(data.data)
-            }
-        })
-    },
-    setStorageSync(key, val, time) {
-        wx.setStorageSync(key, val);
-        var seconds = parseInt(time);
-        if (seconds > 0) {
-            var timestamp = Date.parse(new Date());
-            timestamp = timestamp / 1000 + seconds;
-            wx.setStorageSync(k + 'dtime', timestamp + "")
-        } else {
-            wx.removeStorageSync(k + 'dtime')
-        }
+  onLaunch: function () {
+  },
+  /**
+   * 基础数据
+   */
+  globalData: {
+    vision: '1.0.0',
+    host: 'https://api.darma.cn/mattress',
+    userInfo: null, // {"accessToken":"71bc9ce1c95547459da2f31801f97a08","tokenType":"default","username":"ether"}
+    isLogin: null,
+    deviceNos: '641743000773', // 设备号 641743000773
+    localSocket: {},
+    callback: function () {
     }
+  },
+  onShow: function (options) {
+
+  },
+  /**
+   * 公共异步 一级接口 (不需登录)
+   * @param type 类型： post,get,put,delete
+   * @param url 接口地址
+   * @param data 参数
+   * @param res 成功
+   * @param reg 失败
+   */
+  myAjax(type, url, data, res, reg) {
+    wx.request({
+      url: this.globalData.host + url,
+      data: data,
+      header: {
+        'content-type': 'application/json'
+      },
+      method: type,
+      success(data) {
+        res && res(data.data)
+      },
+      fail(data) {
+        reg && reg(data.data)
+      }
+    })
+  },
+  /**
+   * 公共异步 二级接口 (需登录)
+   * @param type 类型： post,get,put,delete
+   * @param url 接口地址
+   * @param data 参数
+   * @param res 成功
+   * @param reg 失败
+   */
+  myAjax2(type, url, data, res, reg) {
+    let accessToken = Util.getCookie('accessToken');
+    if (!accessToken) {
+      wx.redirectTo({
+        url: '../login/login'
+      })
+    }
+    wx.request({
+      url: this.globalData.host + url,
+      data: data,
+      header: {
+        'content-type': 'application/json',
+        'accessToken': accessToken
+      },
+      method: type,
+      success(data) {
+        res && res(data.data)
+      },
+      fail(data) {
+        reg && reg(data.data)
+      }
+    })
+  },
+  setStorageSync(key, val, time) {
+    wx.setStorageSync(key, val);
+    var seconds = parseInt(time);
+    if (seconds > 0) {
+      var timestamp = Date.parse(new Date());
+      timestamp = timestamp / 1000 + seconds;
+      wx.setStorageSync(k + 'dtime', timestamp + "")
+    } else {
+      wx.removeStorageSync(k + 'dtime')
+    }
+  },
+  showLoad() {
+    if (!isLoading) {
+      wx.showLoading({
+        title: '请稍侯...',
+      });
+      isLoading = true
+    }
+  },
+  hideLoad() {
+    wx.hideLoading();
+    isLoading = false
+  }
 });
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,106 +4,42 @@ const app = getApp();
 let Util = require('../../utils/storage.js');
 
 Page({
-    data: {
-        userInfo: null,
-        deviceNo: '641743000773', // 设备号
-        accessToken: null,
-        breath: null, // 呼吸值 -100_⽆效值，其他为正常值
-        deviceStatus: null, // 设备状态 3_离床，4_在床，5_光纤故障，6_离线，9_传感器负荷，10_信号弱
-        heart: null, // ⼼率值 65436_⽆效值，其他为正常值
-        markTime: null, // 发⽣的时间戳
-        motion: null // 体动值 0_正常，3_轻微体动，4_中度体动，5_⼤幅体动，-100_⽆效值
-    },
-    onLoad() {
-        let accessToken = Util.getCookie('accessToken');
-        let userInfo = Util.getCookie('username');
-        console.log(accessToken);
-        if (!accessToken) {
-            wx.redirectTo({
-                url: '../login/login'
-            })
-        } else {
-            this.setData({
-                userInfo: userInfo
-            });
-            this.getActual();
-            this.setSocketTask()
-        }
-    },
-    setSocketTask() {
-        let socketOpen = false;
-        let socketMsgQueue = ['login', 'deviceStatus', 'healthData', 'paramError'];
-        wx.connectSocket({
-            url: 'ws://stream.darma.cn:18105/ws',
-            header: {
-                'content-type': 'application/json'
-            },
-            method: 'GET',
-            success(res) {
-                console.log(JSON.stringify(res))
-            },
-            fail(reg) {
-                console.log(JSON.stringify(reg))
-            },
-            complete(data) {
-                console.log(JSON.stringify(data))
-            }
-        });
+  data: {
+    userInfo: null
+  },
+  onLoad() {
 
-        wx.onSocketOpen(function (res) {
-            console.log(JSON.stringify(res));
-            socketOpen = true;
-            for (let i = 0; i < socketMsgQueue.length; i++) {
-                sendSocketMessage(socketMsgQueue[i])
-            }
-            socketMsgQueue = []
-        });
-        let _this = this;
-        function sendSocketMessage(msg) {
-            if (socketOpen) {
-                wx.sendSocketMessage({
-                    data: {
-                        msgType: msg,
-                        msgData: {
-                            token: _this.accessToken,
-                            subType: 'ALL',
-                            deviceNo: _this.deviceNo
-                        }
-                    }
-                })
-            } else {
-                socketMsgQueue.push(msg)
-            }
-        }
-    },
-    /**
-     * 获取设备当前的状态/⼼率/呼吸/体动数据
-     */
-    getActual() {
-        let obj = {
-            deviceNos: app.globalData.deviceNos
-        };
-        let _this = this;
-        app.myAjax2('post', '/device/physiology/actual', obj, function (res) {
-            if (res.retCode == '10000') {
-                console.log('请求成功');
-                _this.setData({
-                    breath: res.successData[0].breath,
-                    deviceStatus: res.successData[0].deviceStatus,
-                    heart: res.successData[0].heart,
-                    markTime: res.successData[0].markTime,
-                    motion: res.successData[0].motion
-                });
-            } else {
-                console.log('未知错误，请重新登录')
-            }
-            console.log(JSON.stringify(res))
-        }, function (reg) {
-            console.log(JSON.stringify(reg))
-        })
-    },
-    getDeviceInfo() {
-
-    },
+  },
+  onReady() {
+  },
+  onShow() {
+    // let accessToken = Util.getCookie('accessToken');
+    // let userInfo = Util.getCookie('username');
+    // let _this = this;
+    // if (!accessToken) {
+    //   wx.redirectTo({
+    //     url: '../login/login'
+    //   })
+    // } else {
+    //   this.setData({
+    //     userInfo: userInfo,
+    //     accessToken: accessToken
+    //   });
+    //   _this.getActual();
+    //   // _this.getBatch();
+    //
+    //   // _this.data.timer = setInterval(function () {
+    //   //   _this.getActual()
+    //   // }, 5000);
+    //
+    //   _this.setSocketTask();
+    // }
+  },
+  onHide() {
+    clearInterval(this.data.timer)
+  },
+  onUnload() {
+    clearInterval(this.data.timer)
+  }
 });
 
